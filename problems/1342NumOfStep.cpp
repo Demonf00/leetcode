5bb501abb683decffb0d1c726bcb2b6da3cb4a6c
Demonf00 <freddy.zhao@mail.utoronto.ca>
2022-07-02 15:52:53 -0400

    Add the bitwaise method to 1342NumOfStep, still need optmization

--- problems/1342NumOfStep.cpp
+++ problems/1342NumOfStep.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+    // int numberOfSteps(int num) {
+    //     int current = num;
+    //     int steps = 0;
+    //     while(current != 0)
+    //     {
+    //         if (current % 2)
+    //         {
+    //             current--;
+    //             steps++;
+    //         }
+
+    //         else
+    //         {
+    //             current /= 2;
+    //             steps++;
+    //         }
+    //     }
+    //     return steps;
+    // }
+
+    //using bitwise method
     int numberOfSteps(int num) {
         int current = num;
         int steps = 0;
         while(current != 0)
         {
-            if (current % 2)
-            {
-                current--;
-                steps++;
-            }
-
-            else
-            {
-                current /= 2;
-                steps++;
-            }
+            steps += current & 0x01;
+            current >>= 1;
+            steps++;
         }
         return steps;
     }
